2085438b8831d130fefa95a45c692a728e3a1760
trung0501 <nguyentrungt151@gmail.com>
2025-07-05 11:46:39 +0700

    ex4

--- Ex4.py
+++ Ex4.py
@@ -1,4 +1,3 @@
-# Hàm tính giai thừa
 def factorial(n):
     if n == 0 or n == 1:
         return 1
@@ -10,7 +9,5 @@ group1_permutations = factorial(2)
 group2_positions = 3
 group2_permutations = factorial(3)
 
-# Tổng số cách
 total_ways = group1_positions * group1_permutations * group2_positions * group2_permutations
-
 print("Total number of choices:", total_ways)
